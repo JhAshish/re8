52462c03356bf3c787085e4542848a3b5662c69b
JhAshish <ashishjha432@gmail.com>
2020-11-01 11:16:34 +0530

    separte controller logic /

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//separate controller and logic
+//application logic or controller
+var homepageController = (req, res) => {
+  res.render('index', {title: 'Express' });
+}
+//routing part
+router.get('/', homepageController);
 
 module.exports = router;
