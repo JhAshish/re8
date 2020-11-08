7bc412daf93f2c51b82a0286507fea7870b0b897
JhAshish <ashishjha432@gmail.com>
2020-11-08 15:46:12 +0530

    Add database connection

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,30 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const dbURI = 'mongodb://localhost/re8';
+mongoose.connect(dbURI, { useNewUrlParser: true });
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to ${dbURI}');
+});
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error: ${err}');
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close(() => {
+        console.log('Mongoose disconnected through ${msg}');
+        callback();
+    });
+};
+
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
